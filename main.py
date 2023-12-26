6fd84800ce55cd46e170fb6a361d61f2548a4928
Umar Ali <edumail.umarali@gmail.com>
2023-12-26 15:17:07 +0530

    Completed the Caesar Cipher project.

---
+++ main.py
@@ -0,0 +1,37 @@
+from art import logo
+from os import system, name
+from letters import alphabet
+
+if name == 'nt':
+    system("cls")
+else:
+    system("clear")
+
+print(logo)
+process_active = True
+
+def crypt(message, shift_number, process_type):
+    if shift_number >= 26:
+        shift_number %= 26
+
+    if process_type == 'decode':
+        shift_number = -shift_number
+    
+    new_message = []
+    for letter in message:
+        if letter == ' ':
+            new_message.append(letter)
+        else:
+            new_message.append(alphabet[alphabet.index(letter) + shift_number]) 
+    
+    return ''.join(new_message)
+
+
+while process_active:
+    process_type = input("Type 'encode' to encrypt, type 'decode' to decrypt: ").lower()
+    message = input("Type your message: ").lower()
+    shift_number = int(input("Type the shift number: "))
+    
+    result = crypt(message, shift_number, process_type)
+    print(f"Here's the {process_type}d result: {result}")
+    process_active = False if input("\nType 'yes' if you want to go again, otherwise type 'no': ").lower() == 'no' else True
